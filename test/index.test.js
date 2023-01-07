7a645e93153fa51c868173b38d1ca5ce713dd88e
aytaa <Odak1098!&>
2023-01-07 17:01:47 +0300

    travis unit test2

--- test/index.test.js
+++ test/index.test.js
@@ -6,7 +6,7 @@ describe('#route()', function () {
     context('with destinations', function () {
         it('should compute route', function (done) {
             route.findRoute('New York', 'Los Angeles', process.env.API_KEY, (route) => {
-                console.log('Data => ',route)
+                console.log('Datas => ',route)
                 expect(route).to.include.all.keys('copyrights', 'overview_polyline', 'bounds');
                 done();
             });
