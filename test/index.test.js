8f6e9cf920804b20d52266ebde11eb97119a220e
aytaa <Odak1098!&>
2023-01-07 17:00:40 +0300

    travis unit test

---
+++ test/index.test.js
@@ -0,0 +1,15 @@
+/* test/test.js */
+const route = require('../index');
+const expect = require('chai').expect;
+
+describe('#route()', function () {
+    context('with destinations', function () {
+        it('should compute route', function (done) {
+            route.findRoute('New York', 'Los Angeles', process.env.API_KEY, (route) => {
+                console.log('Data => ',route)
+                expect(route).to.include.all.keys('copyrights', 'overview_polyline', 'bounds');
+                done();
+            });
+        })
+    });
+})
